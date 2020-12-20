5aacb60037de602fa24c3d961bc2ff445dc625d0
MstfOztrk <oztrk.mstf.43@gmail.com>
2020-12-20 18:18:19 +0300

    hazr

--- OtobusBiletUygulaması/Form3.Designer.cs
+++ OtobusBiletUygulaması/Form3.Designer.cs
@@ -42,7 +42,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(98, 219);
+            this.button1.Location = new System.Drawing.Point(106, 219);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(133, 57);
             this.button1.TabIndex = 0;
